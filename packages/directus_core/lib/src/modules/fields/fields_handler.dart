9edbea7fdfd6c94576d1595d81d2cda20fd21c2b
hdbg <httpdebugger@protonmail.com>
2025-08-19 23:46:23 +0300

    feat(fields): added query of specific collection

--- packages/directus_core/lib/src/modules/fields/fields_handler.dart
+++ packages/directus_core/lib/src/modules/fields/fields_handler.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:directus_core/src/data_classes/data_classes.dart';
 import 'package:directus_core/src/modules/fields/field_converter.dart';
 import 'package:directus_core/src/modules/items/items_handler.dart';
 
@@ -6,9 +7,15 @@ import 'directus_field.dart';
 
 class FieldsHandler extends ItemsHandler<DirectusField> {
   FieldsHandler({required Dio client})
-      : super(
-          'directus_fields',
-          client: client,
-          converter: FieldConverter(),
-        );
+    : super('directus_fields', client: client, converter: FieldConverter());
+
+  Future<DirectusListResponse<DirectusField>> readCollectionFields(
+    String collection,
+    String? sort,
+  ) async {
+    return await DirectusListResponse.fromRequest(
+      () => client.get('fields/$collection', queryParameters: {"sort": sort}),
+      converter: converter,
+    );
+  }
 }
