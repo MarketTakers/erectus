4c770277b6ff349ec5d81627d01a6ca4256634ab
hdbg <httpdebugger@protonmail.com>
2025-08-22 18:34:06 +0200

    feat(relations): added api method to get relations of specific collection

--- packages/directus_core/lib/src/modules/relations/relations_handler.dart
+++ packages/directus_core/lib/src/modules/relations/relations_handler.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:directus_core/src/data_classes/data_classes.dart';
 import 'package:directus_core/src/modules/items/items_handler.dart';
 
 import 'directus_relation.dart';
@@ -11,4 +12,23 @@ class RelationsHandler extends ItemsHandler<DirectusRelation> {
           client: client,
           converter: RelationConverter(),
         );
+
+  /// List relations in a specific collection
+  ///
+  /// Returns all relations for a given collection.
+  /// 
+  /// [collection] - The name of the collection to get relations for
+  /// [query] - Optional query parameters for filtering and pagination
+  Future<DirectusListResponse<DirectusRelation>> readByCollection(
+    String collection, {
+    Query? query,
+  }) async {
+    return DirectusListResponse.fromRequest(
+      () => client.get(
+        'relations/$collection',
+        queryParameters: query?.toMap(),
+      ),
+      converter: converter,
+    );
+  }
 }
--- packages/directus_core/test/src/modules/relations/relations_handler_test.dart
+++ packages/directus_core/test/src/modules/relations/relations_handler_test.dart
@@ -1,13 +1,64 @@
+import 'package:directus_core/src/data_classes/data_classes.dart';
 import 'package:directus_core/src/modules/items/items_handler.dart';
 import 'package:directus_core/src/modules/relations/relations_handler.dart';
+import 'package:directus_core/src/modules/relations/directus_relation.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import '../../mock/mock_dio_response.dart';
 import '../../mock/mocks.mocks.dart';
 
 void main() {
-  test('that RelationsHandler extends  ItemsHandler.', () async {
-    final relations = RelationsHandler(client: MockDio());
+  group('RelationsHandler', () {
+    late MockDio client;
+    late RelationsHandler relations;
+    late Map<String, dynamic> exampleRelation;
 
-    expect(relations, isA<ItemsHandler>());
+    setUp(() {
+      client = MockDio();
+      relations = RelationsHandler(client: client);
+      exampleRelation = {
+        'id': 1,
+        'many_collection': 'articles',
+        'many_field': 'author',
+        'one_collection': 'authors',
+        'one_field': 'id',
+      };
+    });
+
+    test('that RelationsHandler extends ItemsHandler.', () async {
+      expect(relations, isA<ItemsHandler>());
+    });
+
+    test('readByCollection works properly', () async {
+      when(client.get(any, queryParameters: anyNamed('queryParameters')))
+          .thenAnswer(dioResponse({
+        'data': [exampleRelation]
+      }));
+
+      final response = await relations.readByCollection('articles');
+
+      expect(response, isA<DirectusListResponse>());
+      expect(response.data, hasLength(1));
+      expect(response.data.first, isA<DirectusRelation>());
+      verify(client.get('relations/articles', queryParameters: null))
+          .called(1);
+    });
+
+    test('readByCollection with query works properly', () async {
+      when(client.get(any, queryParameters: anyNamed('queryParameters')))
+          .thenAnswer(dioResponse({
+        'data': [exampleRelation]
+      }));
+
+      final query = Query(limit: 5);
+      final response = await relations.readByCollection('articles', query: query);
+
+      expect(response, isA<DirectusListResponse>());
+      expect(response.data, hasLength(1));
+      expect(response.data.first, isA<DirectusRelation>());
+      verify(client.get('relations/articles', queryParameters: query.toMap()))
+          .called(1);
+    });
   });
 }
